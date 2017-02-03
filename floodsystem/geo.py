7c605dc27e5e466ec64a1985c68e8992ffd09e53
Minh Nguyen <mhn28@cam.ac.uk>
2017-02-03 00:13:49 +0000

    finished task 1B

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -2,5 +2,26 @@
 geographical data.
 
 """
+from haversine import haversine
+from floodsystem.utils import sorted_by_key
 
-from .utils import sorted_by_key
+def stations_by_distance(station, p):
+    
+    distance_list = []
+
+    for i in station:
+        name = i.name
+        town = i.town
+        coordinate = i.coord
+#Taking the desired values from the list of monitoring stations
+        distance = haversine(coordinate, p)
+        d = (name, town, distance)
+        
+        distance_list.append(d)
+#Putting the desired value into a new list, along with the distance
+    return sorted_by_key(distance_list, 2)
+    
+
+
+
+
