ea42ba41ece39359a1047fad5e21b71cfbad322d
Felix Kok <cyk28@cam.ac.uk>
2017-02-03 00:40:08 +0000

    Complete task 1D

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -2,5 +2,19 @@
 geographical data.
 
 """
+#from floodsystem.utils import sorted_by_key
 
-from .utils import sorted_by_key
+from floodsystem.stationdata import build_station_list
+stations = build_station_list()
+
+def rivers_with_station(stations):
+    river_name = set()
+    for station in stations:
+        river_name.add(station.river)
+    return river_name
+    
+print(rivers_with_station(stations))
+                
+    
+    
+
