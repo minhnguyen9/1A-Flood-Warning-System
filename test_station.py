b2360af4fba28d628d5a3a52801cd59b0c0e4261
Minh Nguyen <mhn28@cam.ac.uk>
2017-02-05 01:55:42 +0000

    Pytest for task 1F

--- test_station.py
+++ test_station.py
@@ -2,6 +2,7 @@
 
 import pytest
 from floodsystem.station import MonitoringStation
+from floodsystem.station import typical_range_consistent
 
 
 def test_create_monitoring_station():
@@ -23,3 +24,22 @@ def test_create_monitoring_station():
     assert s.typical_range == trange
     assert s.river == river
     assert s.town == town
+    
+def test_typical_range_consistent():
+    s_id = "test-s-id"
+    m_id = "test-m-id"
+    label = "some station"
+    coord = (2.0, 4.0)
+    river = "River X"
+    town = "My Town1"
+    trange1 = (2.3, 3.4445)
+    trange2 = (None)
+    trange3 = (2, 0.5)
+    s1 = MonitoringStation(s_id, m_id, label, coord, trange1, river, town)
+    s2 = MonitoringStation(s_id, m_id, label, coord, trange2, river, town)
+    s3 = MonitoringStation(s_id, m_id, label, coord, trange3, river, town)
+    s = (s1, s2, s3)
+    a = typical_range_consistent(s)
+    assert type(a) == list
+    assert type(a[0]) == str
+    assert len(a) == 2
