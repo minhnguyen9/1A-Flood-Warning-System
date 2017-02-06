0495c0a131098faddd92d77ceef93c820f99cf7e
Felix Kok <cyk28@cam.ac.uk>
2017-02-06 23:07:45 +0000

    Complete Task 1D and 1E

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -2,21 +2,12 @@
 geographical data.
 
 """
-
-from floodsystem.stationdata import build_station_list
-stations = build_station_list()
-
-def rivers_with_station(stations):
-    river_name = set()
-    for station in stations:
-        river_name.add(station.river)
-    return river_name
-    
-print(rivers_with_station(stations))
                 
 from haversine import haversine
 from floodsystem.utils import sorted_by_key
+from floodsystem.stationdata import build_station_list
 
+#For Task1B
 def stations_by_distance(station, p):
     
     distance_list = []
@@ -33,5 +24,80 @@ def stations_by_distance(station, p):
 #Putting the desired value into a new list, along with the distance
     return sorted_by_key(distance_list, 2)
     
+stations = build_station_list()
+
+#For Task1D
+def rivers_with_station(stations): 
+    
+    #create empty set and list
+    river_set = set()
+    river_list = []
+    
+    # Add river names into set (prevent repeats)
+    for station in stations:
+        river_set.add(station.river)
+        
+    # Convert the set to a list
+    for river in river_set:
+        river_list.append(river)
+    river_list.sort()
+    return river_list
+
+def stations_by_river(stations):
+    
+    # Create empty dict and list of rivers using the previous function
+    river_keys = rivers_with_station(stations)
+    stations_by_river_dict = {}
+    
+    for river in river_keys:
+        # Store river names as keys and empty lists as values
+        stations_by_river_dict[river] = []
+        for station in stations:
+            if station.river == river:
+                stations_by_river_dict[river].append(station.name)
+            else :
+                pass
+        stations_by_river_dict[river].sort()
+    return stations_by_river_dict
 
 
+#For Task 1E
+def rivers_by_station_number(stations, N):
+    # Define variables and create empty list
+    river_names = rivers_with_station(stations)
+    n = 0
+    list_of_tuples = []
+    
+    for river in river_names:
+        
+        # Create empty list and store river names into first element of each small list
+        list_1 = []
+        list_1.append(river)
+        n = 0
+        
+        # Iterate through list of stations' rivers and count the number of rivers
+        for station in stations:
+            if station.river == river:
+                n += 1
+            else:
+                pass
+        list_1.append(n)
+        
+        # Convert a list of lists to a list of tuples
+        tup = tuple(list_1)
+        list_of_tuples.append(tup)
+        
+    # Sort the list of tuples by their second elements    
+    list_of_tuples = sorted(list_of_tuples, key=lambda x: x[1], reverse = True)
+    
+    # For special cases where there are more rivers with the same number of stations as the N th entry
+    list_of_lists = []
+    for t in list_of_tuples:
+        list_of_lists.append(list(t))
+    while N < len(list_of_lists):
+        if list_of_lists[N-1][1] == list_of_lists[N][1]:
+            N += 1
+        else:
+            break
+    list_of_tuples = list_of_lists[:N]
+    return list_of_tuples
